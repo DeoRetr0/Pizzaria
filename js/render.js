ece7e8de49e97ee7ef03f7103083c5c523a3c461
Josué de Paulo Viana <josuep.viana@gmail.com>
2020-02-09 00:48:05 -0300

    AJAX IMPLEMENTADO
    Menu e Footer -> Não responsivos!

--- js/render.js
+++ js/render.js
@@ -1,16 +1,3 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
-
-function load_page(arquivo){
-  if(arquivo){
-    $.ajax({
-      type: 'GET',
-      data: arquivo,
-      url: arquivo,
-      success: function(data){
-        $("#pagina_retorno").html(data);
-      }
-    });
-  }
-}
